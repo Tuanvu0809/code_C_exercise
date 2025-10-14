bbd5c8f12236984ee6ff75b2cc9d09885bd1e912
Lê Tuấn Vũ <tuanvu0809@gmail.com>
2025-10-14 22:42:26 +0700

    ex1

--- codeC/ex1.c
+++ codeC/ex1.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
+/*bat 1 bit*/
+#define ON_BIT_N(A,x)       A= A|(1<<x) 
+#define OFF_BIT_N(A,x)      A = A&(~(1<<x))
+
+void printBinaryCompact(unsigned int n) {
+    printf("\t0x");
+    for(int i=7; i>=0;i--)    (n >> i) & 1 ? printf("1"): printf("0"); 
+    printf("\n");
+}
+
 int main()
 {
-    printf("hello\n");
+    int a= 0;
+    printf("a= %d\t ",a);
+    printBinaryCompact(a);
+    ON_BIT_N(a,1);
+    printf("a= %d\t ",a);
+    printBinaryCompact(a);
+    OFF_BIT_N(a,1);
+    printf("a= %d\t ",a);
+    printBinaryCompact(a);
+  
 
     return 0;
 }
